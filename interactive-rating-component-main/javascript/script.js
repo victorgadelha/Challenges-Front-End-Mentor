6ec437bc68a0e32ea831e0b34a1a3736590e6a61
João Victor Gadelha <108595157+victorgadelha@users.noreply.github.com>
2023-07-10 22:09:38 -0300

    feat: fazendo segundo card desaparecer ao clicar no botão de submit.

--- interactive-rating-component-main/javascript/script.js
+++ interactive-rating-component-main/javascript/script.js
@@ -1,3 +1,12 @@
 const rate = document.querySelectorAll('.rate')
 const button = document.querySelector('#submit-button')
+const firstCard = document.querySelector('.rating-container')
+const secondCard = document.querySelector('.greetings-container')
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    firstCard.classList.toggle('disabled')
+    secondCard.classList.toggle('disabled')
+})
+
 
