7edd594340cff028fa55552ff8ea3ded84c91a8c
João Victor Gadelha <108595157+victorgadelha@users.noreply.github.com>
2023-07-11 21:11:05 -0300

    feat: adicionando toggle de cor para a nota.
    Ao clicar na nota, ela recebe a cor cinza claro. Se clicar em outra, a que estiver cinza claro volta a cor original e a clicada recebe a cor cinza.

--- interactive-rating-component-main/javascript/script.js
+++ interactive-rating-component-main/javascript/script.js
@@ -1,12 +1,25 @@
-const rate = document.querySelectorAll('.rate')
-const button = document.querySelector('#submit-button')
-const firstCard = document.querySelector('.rating-container')
-const secondCard = document.querySelector('.greetings-container')
+const rate = document.querySelectorAll('.rate');
+const button = document.querySelector('#submit-button');
+const firstCard = document.querySelector('.rating-container');
+const secondCard = document.querySelector('.greetings-container');
 
-button.addEventListener('click', (e) => {
-    e.preventDefault()
-    firstCard.classList.toggle('disabled')
-    secondCard.classList.toggle('disabled')
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    firstCard.classList.toggle('disabled');
+    secondCard.classList.toggle('disabled');
 })
 
+rate.forEach(rateElement => rateElement.addEventListener("click", toggleColor));
 
+function toggleColor() {
+  const clickedElement = this;
+  
+  rate.forEach(rateElement => {
+    if (rateElement === clickedElement) {
+      rateElement.style.color = "var(--White)";  
+      rateElement.style.backgroundColor = "var(--Light-Grey)";
+    } else {
+      rateElement.style.backgroundColor = "";
+    }
+  });
+}
